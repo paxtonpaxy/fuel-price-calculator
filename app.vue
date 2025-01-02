f50852ee122549fe3acfc2ad64a42dd931369363
Ivan <risticaivan@gmail.com>
2025-01-02 21:52:33 +0100

    small update

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
 </template>
 
 <style scoped lang="scss">
-@import './assets/css/main.css';
+@import "./assets/css/main.css";
 
 .app {
   height: 100vh;
@@ -13,5 +13,7 @@
   background: linear-gradient(45deg, rgb(12, 12, 12), rgb(36, 36, 36));
   display: flex;
   justify-content: center;
+  align-items: center;
+  gap: 2rem;
 }
 </style>
